e9c94d3003fb79c125e94c36a6d071ec1d092e85
mortegac <mortega@apgca.cl>
2021-03-20 15:14:30 +0000

    feat: changes view login

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,32 +1,74 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handlerClick = e => {
+		e.preventDefault();
+
+		actions.setLogin({
+			email: email,
+			password: password
+		});
+	};
+
+	useEffect(() => {
+		actions.getToken();
+	}, []);
 
 	return (
 		<div className="row w-100 d-flex justify-content-center">
 			<div className="col-5">
-				<div className="text-center mt-5">
-					<h1>Ingrese sus acccesos</h1>
+				{store.user.token !== null ? (
+					<div className="text-center mt-5">
+						{/* <span>User: {JSON.stringify(store.user)}</span> */}
+						<div className="card" style={{ width: "18rem" }}>
+							<img
+								className="card-img-top"
+								src="https://cdn.generadormemes.com/media/templates/xllama_fea.jpg.pagespeed.ic.plantilla-memes.jpg"
+							/>
+							<div className="card-body">
+								<span>User: {JSON.stringify(store.user)}</span>
+							</div>
+						</div>
+					</div>
+				) : (
+					<div className="text-center mt-5">
+						<h1>Ingrese sus acccesos</h1>
 
-					{/* <div className="alert alert-info">{store.message || "Loading message from the backend..."}</div> */}
-					<form>
-						<div className="mb-3">
-							<label className="form-label">Email address</label>
-							<input type="email" className="form-control" aria-describedby="emailHelp" />
-						</div>
-						<div className="mb-3">
-							<label className="form-label">Password</label>
-							<input type="password" className="form-control" />
-						</div>
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</form>
-				</div>
+						<form>
+							<div className="mb-3">
+								<label className="form-label">Email address</label>
+								<input
+									value={email}
+									onChange={e => setEmail(e.target.value)}
+									type="email"
+									className="form-control"
+								/>
+							</div>
+							<div className="mb-3">
+								<label className="form-label">Password</label>
+								<input
+									value={password}
+									onChange={e => setPassword(e.target.value)}
+									type="password"
+									className="form-control"
+								/>
+							</div>
+							<button type="submit" onClick={e => handlerClick(e)} className="btn btn-primary">
+								Submit
+							</button>
+							{store.user !== "" ? (
+								<div className="alert alert-info overflow-auto">{JSON.stringify(store.user.token)}</div>
+							) : null}
+						</form>
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,47 +1,83 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			user: {
+				expires: "",
+				token: "",
+				username: "",
+				email: "",
+				userId: "",
+				userName: ""
+			}
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getToken: () => {
+				const tokenLocal = localStorage.getItem("token");
+				const userLocal = JSON.parse(localStorage.getItem("user"));
+				setStore({
+					user: {
+						token: tokenLocal,
+						user: userLocal
+					}
+				});
+				console.log("-->", tokenLocal);
+				console.log("-->", JSON.stringify(userLocal));
+			},
+			setLogin: user => {
+				const additionalSettings = {
+					method: "POST",
+					header: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(user)
+				};
+
+				fetch(process.env.BACKEND_URL + "/api/login", {
+					method: "POST",
+					body: JSON.stringify(user),
+					headers: { "Content-type": "application/json; charset=UTF-8" }
+				})
+					.then(resp => resp.json())
+					.then(data => {
+						console.log("--data--", data);
+						setStore({ user: data });
+
+						if (typeof Storage !== "undefined") {
+							localStorage.setItem("token", data.token);
+							localStorage.setItem("user", JSON.stringify(data.user));
+						} else {
+							// LocalStorage no soportado en este navegador
+						}
+					})
+					.catch(error => console.log("Error loading message from backend", error));
 			},
 
 			getMessage: () => {
-				// fetching data from the backend
 				fetch(process.env.BACKEND_URL + "/api/hello")
 					.then(resp => resp.json())
 					.then(data => setStore({ message: data.message }))
 					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
+
+			// Use getActions to call a function within a fuction
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
 		}
 	};
 };
